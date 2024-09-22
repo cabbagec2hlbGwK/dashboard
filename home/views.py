3e2653be2425449ea9026a4887798a9cd43f8493
asher james <cabbageshell@protonmail.com>
2024-09-22 19:35:50 -0400

    cjamge in the value

--- home/views.py
+++ home/views.py
@@ -237,13 +237,15 @@ def search(res):
 
 # Create your views here.
 def loginUser(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        # Redirect to a success page.
-        ...
-    else:
-        # Return an 'invalid login' error message.
-        ...
+    input(request.method)
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            ...
+        else:
+            # Return an 'invalid login' error message.
+            ...
