f9cddac7bb746285511bd3e1fba92a8c40adbfac
asher james <cabbageshell@protonmail.com>
2024-09-22 19:45:01 -0400

    cjamge in the value

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -245,9 +245,8 @@ def loginUser(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # Redirect to a success page.
+            return redirect("home")
         else:
-            pass
-            # Return an 'invalid login' error message.
+            return render(request, "login.html", {"error": "Invalid credentials"})
     else:
         return render(request, "login.html")
