d20730646c93632d707a26877a133cb949b3e6a7
asher james <cabbageshell@protonmail.com>
2025-01-19 14:43:00 -0500

    change in the fs

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -12,9 +12,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# load env
+try:
+    load_dotenv(dotenv_path=os.path.join(BASE_DIR,".env.local"))
+except Exception as e:
+    print(e)
+
 
 
 # Quick-start development settings - unsuitable for production
