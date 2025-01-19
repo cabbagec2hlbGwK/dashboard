b7a40cba1961c479c7ca6691cf08dd88a466961a
asher james <cabbageshell@protonmail.com>
2025-01-19 13:01:55 -0500

    working on the pro config

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "theme/static"
+STATIC_URL = "theme/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 SESSION_COOKIES_SECURE= True
